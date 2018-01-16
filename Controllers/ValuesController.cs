bbb5eaf0664f241650e220a2888e0bfb05c2f1a3
HansJao <vigorhan@gmail.com>
2018-01-16 18:12:55 +0800

    init

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using Line.Messaging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Line.Messaging.Webhooks;
+using System.Net;
+
+namespace HYLineWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        static LineMessagingClient lineMessagingClient;
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //lineMessagingClient = new LineMessagingClient("VO8CJj2Uwn2h5Mjm4884whpRKOXonme17QnbPQXatFKIDckf33rFM8jL+8Qv0hCPY0unc80NrZiWKR/Ut4qv1gSuRUAYdXZwMhctijKzqsVRbVD3Vm1STrcdMQzzu0QKeTjd/5pFDHF6jc9w35OKbwdB04t89/1O/w1cDnyilFU=");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // // POST api/values
+        // [HttpPost]
+        // public void Post([FromBody]string value)
+        // {
+        // }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+         public ValuesController()
+        {
+            lineMessagingClient = new LineMessagingClient("VO8CJj2Uwn2h5Mjm4884whpRKOXonme17QnbPQXatFKIDckf33rFM8jL+8Qv0hCPY0unc80NrZiWKR/Ut4qv1gSuRUAYdXZwMhctijKzqsVRbVD3Vm1STrcdMQzzu0QKeTjd/5pFDHF6jc9w35OKbwdB04t89/1O/w1cDnyilFU=");
+        }
+
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post(HttpRequestMessage request)
+        {
+            var events = await request.GetWebhookEventsAsync("c1f910527e6456141087387d2ce2b783");
+            //lineMessagingClient = new LineMessagingClient("VO8CJj2Uwn2h5Mjm4884whpRKOXonme17QnbPQXatFKIDckf33rFM8jL+8Qv0hCPY0unc80NrZiWKR/Ut4qv1gSuRUAYdXZwMhctijKzqsVRbVD3Vm1STrcdMQzzu0QKeTjd/5pFDHF6jc9w35OKbwdB04t89/1O/w1cDnyilFU=");
+            LinebotApp app = new LinebotApp(lineMessagingClient);
+            await app.RunAsync(events);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);//test
+            return response;
+        }      
+    }
+}
