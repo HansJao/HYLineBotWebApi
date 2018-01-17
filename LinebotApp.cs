fe1a17ade0fcc38b8b351cc84ba17eebd1d31e32
HansJao <vigorhan@gmail.com>
2018-01-18 01:03:20 +0800

    分頁第二版

--- LinebotApp.cs
+++ LinebotApp.cs
@@ -46,7 +46,7 @@ namespace HYLineWebApi.Controllers
                 case "help":
                     await messagingClient.ReplyMessageAsync(mev.ReplyToken, new List<ISendMessage>
                     {
-                        new TextMessage("======查詢指令======\n ?倉庫 [倉庫名稱] \n ?名稱 [布種名稱] \n ======新增指令======\n + [倉庫名稱] [布種名稱] [顏色] [儲位] [數量] [備註] \n ======修改指令======\n ! [顆顆,還沒做]\n ======刪除指令======\n - [編號]")
+                        new TextMessage("======查詢指令!======\n ?倉庫 [倉庫名稱] \n ?名稱 [布種名稱] \n ======新增指令======\n + [倉庫名稱] [布種名稱] [顏色] [儲位] [數量] [備註] \n ======修改指令======\n ! [顆顆,還沒做]\n ======刪除指令======\n - [編號]")
                     });
                     break;
                 default:
@@ -87,12 +87,12 @@ namespace HYLineWebApi.Controllers
             if(column.Last().Actions.Count() != 3)
             {
                 column.Last().Actions.Add(new MessageTemplateAction("下一頁", string.Concat("?倉庫 ", messageSpilt[1]," ", defaultPage + 1)));
-            }else
+            }
+            else
             {
                 column.Add(new CarouselColumn("欲搜尋下一頁請點選", actions: new List<ITemplateAction>()
                 {
-                   new MessageTemplateAction("下一頁", string.Concat("?倉庫 ", messageSpilt[1]," ", defaultPage + 1)),
-                   new MessageTemplateAction("", string.Concat("?倉庫 ", messageSpilt[1]," ", defaultPage + 1))
+                   new MessageTemplateAction("下一頁", string.Concat("?倉庫 ", messageSpilt[1]," ", defaultPage + 1))
                 }));
             }
 
